b592b4f635d5d18db5818cc5ee81495dff8c93b5
viktoriya <victoria.dimitrova0@gmail.com>
2025-03-27 11:07:12 +0200

    changes to build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,14 @@
 plugins {
     kotlin("jvm") version "1.9.0"
-    `java-library`
-    `maven-publish`
 }
 
-group = "com.savvy"
+group = "com.github.Vikk11"
 version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
-java {
-    withSourcesJar()
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            groupId = "com.savvy"
-            artifactId = "common-models"
-            version = "1.0.0"
-        }
-    }
-    repositories {
-        maven {
-            url = uri("file:///app/common-models-repo")
-        }
-    }
+dependencies {
+    implementation(kotlin("stdlib"))
 }
